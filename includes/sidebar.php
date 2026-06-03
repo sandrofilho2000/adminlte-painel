40cac68def43aa62e089779d71900780a9ffffab
Sandro Filho <sandro.afonso.info@confef.org.br>
2026-06-03 17:03:27 -0300

    .

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <a href="/admin" class="brand-link">
+            <span class="brand-text font-weight-light">
+                Meu Sistema
+            </span>
+        </a>
+
+        <div class="sidebar">
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview">
+                    <li class="nav-item">
+                        <a href="/admin" class="nav-link">
+                            <i class="nav-icon fas fa-home"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="/usuarios" class="nav-link">
+                            <i class="nav-icon fas fa-users"></i>
+                            <p>Usuários</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="/configuracoes" class="nav-link">
+                            <i class="nav-icon fas fa-cogs"></i>
+                            <p>Configurações</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </aside>
